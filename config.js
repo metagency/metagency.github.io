62733365be2a98a5ed03f4b915d97c403d94cae9
metagency <97617826+metagency@users.noreply.github.com>
2022-01-12 17:10:33 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+  chain : '0x5',
+  chainName: 'Goerli',
+  //saleContractAddress: '0x339de1C8b027A8cC966B12fDFD51eeD1E76D4D2C'
+  saleContractAddress: '0xa8754c14376b4fbe1A25C2eA50E225Ac368368E0'
+};
