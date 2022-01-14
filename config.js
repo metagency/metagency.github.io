972b4a5b08fb2a354d3eb3e3aa0fcd149e404942
metagency <97617826+metagency@users.noreply.github.com>
2022-01-14 10:46:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-  chain : '0x5',
-  chainName: 'Goerli',
-  saleContractAddress: '0x98F52f03e703A3d5C08f308e2AB6E5bBedc8024e'
+  chain : '0x1',
+  chainName: 'Mainnet',
+  saleContractAddress: '0x177938C713D0fDef8a47ece84eBB2718d01073a8'
 };
